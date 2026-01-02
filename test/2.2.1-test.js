02a4dc3963714af0b00088268648a61fe9df187e
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2026-01-02 22:35:42 +0900

    feat: 「2.2.1 ひらがなと漢字の使い分け」のルールを更新

--- test/2.2.1-test.js
+++ test/2.2.1-test.js
@@ -5,7 +5,7 @@ import rule from "../src/2.2.1";
 
 const tester = new TextLintTester();
 tester.run("2.2.1.ひらがなと漢字の使い分け", rule, {
-    valid: ["問題は以下のとおりです"],
+    valid: ["問題は以下のとおりです", "ときどき確認する", "彼のために尽くす"],
     invalid: [
         {
             text: "問題は以下の通り",
@@ -14,6 +14,26 @@ tester.run("2.2.1.ひらがなと漢字の使い分け", rule, {
                 {
                     message: `以下の通り => 以下のとおり`,
                     index: 3
+                }
+            ]
+        },
+        {
+            text: "到達する為に",
+            output: "到達するために",
+            errors: [
+                {
+                    message: "為に => ために",
+                    index: 4
+                }
+            ]
+        },
+        {
+            text: "彼の為に",
+            output: "彼のために",
+            errors: [
+                {
+                    message: "の為 => のため",
+                    index: 1
                 }
             ]
         }
